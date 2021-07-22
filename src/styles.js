8dd548db22f74db03e6eaa4a51900f012c238c9d
Rafael Peruggia <perugiar@gmail.com>
2021-07-22 09:18:13 -0300

    chore(files): removing some files and moving style

---
+++ src/styles.js
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react';
+
+export const globals = (theme) => css`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+      'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+      'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: ${theme.backgroundColor};
+    color: ${theme.color};
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`;
+
+export const darkTheme = {
+  backgroundColor: '#282c34',
+  color: 'white',
+};
